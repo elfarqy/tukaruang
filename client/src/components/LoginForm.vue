13f242441a268c6b5300fa7b61a74c91197c2ed8
elfarqy <alvianthelfarqy@gmail.com>
2023-10-04 21:20:06 +0700

    setup login and logout

---
+++ client/src/components/LoginForm.vue
@@ -0,0 +1,52 @@
+<!-- src/components/LoginForm.vue -->
+
+
+<script>
+import axios from "axios";
+// import { useRouter } from 'vue-router';
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+  async login() {
+    try {
+      const response = await axios.post("http://kb.ved/api/login", {
+        username: this.username,
+        password: this.password,
+      });
+      if (response.status == 200) {
+          console.log(response.data.data.user.token);
+          const token = response.data.data.user.token;
+
+          localStorage.setItem('token', token);
+          this.$router.push('/dashboard');
+          window.location.reload();
+        } else {
+          console.error('Login failed');
+        }
+    } catch (error) {
+      console.error(error);
+    }
+  },
+}
+};
+</script>
+
+<template>
+  <div class="login-form">
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <input type="text" v-model="username" placeholder="Username" required />
+      <input type="password" v-model="password" placeholder="Password" required />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+/* Add your CSS styles here */
+</style>
---
+++ client/src/components/LogoutPage.vue
@@ -0,0 +1,19 @@
+<!-- Logout.vue -->
+<template>
+    <div>
+      <button @click="logout">Logout</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      logout() {
+        localStorage.removeItem('token');
+        this.$router.push('/login');
+        window.location.reload();
+      },
+    },
+  };
+  </script>
+  
